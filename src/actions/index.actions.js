0e0980094626cfe20ac3ace64096d959aa771d62
lekegitrepo <lekedevrepo@gmail.com>
2020-08-09 16:08:03 -0500

    create index actions

---
+++ src/actions/index.actions.js
@@ -0,0 +1,24 @@
+const actions = {
+  ADD_RECIPES: 'ADD_RECIPES',
+  ADD_CATEGORIES: 'ADD_CATEGORIES',
+  CHANGE_FILTER: 'CHANGE_FILTER',
+};
+
+const addRecipes = recipes => (
+  {
+    type: actions.ADD_RECIPES,
+    recipes,
+  }
+);
+
+const addCategories = categories => ({
+  type: actions.ADD_CATEGORIES,
+  categories,
+});
+
+const changeFilter = filter => ({
+  type: actions.CHANGE_FILTER,
+  filter,
+});
+
+export { actions, addCategories, changeFilter, addRecipes}
