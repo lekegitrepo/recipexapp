3e49724316f973d18be66f8f1d2c67c5d398ce8b
lekegitrepo <lekedevrepo@gmail.com>
2020-08-11 03:32:09 -0500

    create Recipe component

---
+++ src/components/Recipe.component.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+
+class Recipe extends Component {
+  render() {
+    const { recipes, location } = this.props;
+    const recipe = recipes.find(item => item.strMeal === location.state.recipe);
+    const displayRecipe = recipe ? (
+      <div className="container">
+        <div className="recipe" style={{ marginTop: '40px' }}>
+          <img className="recipe-image" src={recipe.strMealThumb} alt={recipe.strMeal} style={{ width: '350px' }} />
+          <h3 className="recipe-title">{recipe.strMeal}</h3>
+          <h4 className="recipe-details">
+          Category:
+            <span>{recipe.strCategory}</span>
+          </h4>
+          <p className="recipe-website">
+          YouTube:
+            <span><a href={recipe.strYoutube}>{recipe.strMeal}</a></span>
+          </p>
+          <button className="btn btn-back" type="button">
+            <Link to="/">Back</Link>
+          </button>
+        </div>
+      </div>
+    ) : <h4>Recipe Not Found</h4>;
+    return displayRecipe;
+  }
+}
+
+Recipe.propTypes = {
+  recipes: PropTypes.instanceOf(Array).isRequired,
+  location: PropTypes.instanceOf(Object).isRequired,
+};
+
+const mapStateToProps = ({ recipes }) => ({
+  recipes,
+});
+
+export default connect(mapStateToProps)(Recipe);
