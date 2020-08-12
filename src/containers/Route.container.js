bdbfb9ef9b36463994f959c42b24c6ac1a6bf81c
lekegitrepo <lekedevrepo@gmail.com>
2020-08-11 22:27:42 -0500

    create Route container

---
+++ src/containers/Route.container.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store/index.store';
+import App from '../components/App';
+import Recipes from '../components/Recipes.component';
+import Recipe from '../components/Recipe.component';
+
+const PageRoute = () => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />  
+        <Route path="/category/:id" component={Recipes} />
+        <Route path="/recipe/:id" component={Recipe} />  
+      </Switch>
+    </Router>
+  </Provider>
+
+);
+
+export default PageRoute;
